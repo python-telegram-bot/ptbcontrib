c5efd66fa7b8eea3bd1d8bec001cabdf17909821
Bibo-Joshi <hinrich.mahler@freenet.de>
2021-01-03 10:53:12 +0100

    ReplyToMessageFilter (#14)
    * ReplyToMessageFilter
    
    * Codacy
    
    * Update readme

---
+++ ptbcontrib/reply_to_message_filter/README.md
@@ -0,0 +1,35 @@
+# Apply Filters to `update.effective_message.reply_to_message`
+
+Provides a class `ReplyToMessageFilter` that allows you to apply Filters to `update.effective_message.reply_to_message`.
+
+```python
+from telegram.ext import Filters, MessageHandler
+from ptbcontrib.reply_to_message_filter import ReplyToMessageFilter
+    
+# accepts only messages that are replies to text messages
+replies_to_text = MessageHandler(
+    ReplyToMessageFilter(Filters.text),
+    callback
+)
+    
+# accepts only messages that are replies to non-sticker messages
+replies_to_anything_but_stickers = MessageHandler(
+    ~ReplyToMessageFilter(Filters.sticker),
+    callback
+)
+    
+# accepts only messages containing documents that are replies to
+# a message containing a document
+documents_as_reply_to_documents = MessageHandler(
+    Filters.document & ReplyToMessageFilter(Filters.document),
+    callback
+)
+```
+
+## Requirements
+
+*   `python-telegram-bot>=13.0`
+
+## Authors
+
+*   [Hinrich Mahler](https://github.com/bibo-joshi)
---
+++ ptbcontrib/reply_to_message_filter/replytomessagefilter.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#
+# A library containing community-based extension for the python-telegram-bot library
+# Copyright (C) 2020-2021
+# The ptbcontrib developers
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser Public License for more details.
+#
+# You should have received a copy of the GNU Lesser Public License
+# along with this program.  If not, see [http://www.gnu.org/licenses/].
+"""This module contains a filter class that applies filters to Message.reply_to_message."""
+from typing import Dict, Optional, Union
+
+from telegram import Update
+from telegram.ext import BaseFilter, UpdateFilter
+
+
+class ReplyToMessageFilter(UpdateFilter):
+    """
+    Applies filters to ``update.effective_message.reply_to_message``.
+
+    Args:
+        filters (:class:`telegram.ext.BaseFilter`): The filters to apply. Pass exactly like passing
+            filters to :class:`telegram.ext.MessageHandler`.
+
+    Attributes:
+        filters (:class:`telegram.ext.BaseFilter`): The filters to apply.
+    """
+
+    def __init__(self, filters: BaseFilter):
+        self.filters = filters
+        self.data_filter = self.filters.data_filter
+
+    def filter(self, update: Update) -> Optional[Union[bool, Dict]]:
+        if not update.effective_message.reply_to_message:
+            return False
+
+        reply_to_message = update.effective_message.reply_to_message
+        if update.channel_post:
+            return self.filters(Update(1, channel_post=reply_to_message))
+        if update.edited_channel_post:
+            return self.filters(Update(1, edited_channel_post=reply_to_message))
+        if update.message:
+            return self.filters(Update(1, message=reply_to_message))
+        if update.edited_message:
+            return self.filters(Update(1, edited_message=reply_to_message))
+        return False
