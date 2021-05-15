50c46eb0b92b00de0ed8ce6422ed57f5923f178b
allerter <45076212+allerter@users.noreply.github.com>
2021-05-15 17:30:09 +0430

    get_chat_link (#21)
    * added chat_to_link
    
    * fix numbering in docstring
    
    * updated docstring
    
    * updated readme
    
    * added tests
    
    * fixed typo in tests
    
    * Return username as a URL like other links
    
    Co-authored-by: Poolitzer <25934244+Poolitzer@users.noreply.github.com>
    
    * Update ptbcontrib/chat_to_link/README.md
    
    Co-authored-by: Poolitzer <25934244+Poolitzer@users.noreply.github.com>
    
    * import BadRequest directly
    
    * remove create_invite_link
    
    * remove timeout and api_kwargs parameters
    
    * updated docstring
    
    * correct typo in docstring
    
    * updated readme
    
    * removed create_chat_link tests
    
    * tests: updated test_chat_username to assert returned link is URL
    
    * tests: removed unused member_limit parameter
    
    * fix numbering in docstring
    
    * removed extra space before bullet in readme
    
    * fix: trying to make codacy happy
    
    lets see if that is what it wants
    
    * Use chat.link instead of creating URL from username
    
    * return None instead of empty string
    
    * Update README and dostrings
    
    * convert error message to lowercase
    
    * add maximum api calls warning to readme
    
    * tests: update test_bot_permission_error
    
    * update chat_to_link.py
    
    - Define function on one line
    - Instead of seeing if the error message matches, check to see if it has the phrase "not enough rights" in it
    
    * update docstring and readme
    
    Co-Authored-By: Bibo-Joshi <22366557+Bibo-Joshi@users.noreply.github.com>
    
    * rename chat_to_link to get_chat_link
    
    Co-authored-by: Poolitzer <25934244+Poolitzer@users.noreply.github.com>
    Co-authored-by: Poolitzer <mail@poolitzer.eu>
    Co-authored-by: allerter <8884518-allerter@users.noreply.gitlab.com>
    Co-authored-by: Bibo-Joshi <22366557+Bibo-Joshi@users.noreply.github.com>

---
+++ ptbcontrib/get_chat_link/README.md
@@ -0,0 +1,26 @@
+# Get joinable link from Chat
+
+Provides a method to get a joinable link from a `telegram.Chat` object. To do so, it tries
+to get a link in the following order:
+1.  Chat's link (`chat.link`).
+
+2.  Chat's invite link (`chat.invite_link`).
+
+3.  Chat's invite link from bot (`bot.get_chat(chat.id).invite_link`).
+
+4.  Export primary invite link (`bot.export_chat_invite_link`).
+
+5.  `None` since there is no valid link and the bot doesn't have permission
+   to create one either.
+
+**Warning**: This function might make up to 2 API calls to get a valid chat link.
+
+Please see the docstrings for more details.
+
+## Requirements
+
+*   `python-telegram-bot>=13.4`
+
+## Authors
+
+*   [Allerter](https://github.com/allerter)
---
+++ ptbcontrib/get_chat_link/get_chat_link.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#
+# A library containing community-based extension for the python-telegram-bot library
+# Copyright (C) 2020-2021
+# The ptbcontrib developers
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser Public License for more details.
+#
+# You should have received a copy of the GNU Lesser Public License
+# along with this program.  If not, see [http://www.gnu.org/licenses/].
+"""This module contains a helper function to get joinable links from chats."""
+from typing import Optional
+from telegram import Chat
+from telegram.error import BadRequest
+
+
+def get_chat_link(chat: Chat) -> Optional[str]:
+    """
+    Gets a link for the chat in the following order if the link is valid:
+     1. Chat's link (`chat.link`).
+     2. Chat's invite link (`chat.invite_link`).
+     3. Chat's invite link from bot (`bot.get_chat(chat.id).invite_link`)
+     4. Export primary invite link (`bot.export_chat_invite_link`).
+     5. ``None`` since there is no valid link and the bot doesn't have permission
+        to create one either.
+
+    This function doesn't check to see if the bot has enough permissions to create/export
+    invite links. Instead, it makes the API call and relies on the response (new link or
+    error message) to determine the outcome. This way we can avoid an extra API call.
+
+    Warning:
+        This function might make up to 2 API calls to get a valid chat link.
+
+    Args:
+        chat (:obj:`telegram.Chat`): The chat to get a link from.
+
+    Returns:
+        :obj:`Optional[str]`: Chat link as a URL if there is any. Otherwise None.
+    """
+    bot = chat.bot
+    if chat.link:
+        return chat.link
+    if chat.invite_link:
+        return chat.invite_link
+
+    bot_chat = bot.get_chat(chat.id)
+    if bot_chat.invite_link:
+        return bot_chat.invite_link
+
+    try:
+        return chat.export_invite_link()
+    except BadRequest as exc:
+        if "not enough rights" in exc.message.lower():
+            return None
+        raise exc
