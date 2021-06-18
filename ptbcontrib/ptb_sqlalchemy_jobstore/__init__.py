48ff1ea78a89197f7b307a0c1d0be8c57c531620
starry69 <starry369126@outlook.com>
2021-06-18 09:29:57 +0530

    oh no
    Signed-off-by: starry69 <starry369126@outlook.com>

--- ptbcontrib/ptb_sqlalchemy_jobstore/__init__.py
+++ ptbcontrib/ptb_sqlalchemy_jobstore/__init__.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU Lesser Public License
 # along with this program.  If not, see [http://www.gnu.org/licenses/].
-"""This module contains AdaptedSQLAlchemyJobStore."""
+"""This module contains PTBSQLAlchemyJobStore."""
 from .jobstore import PTBSQLAlchemyJobStore
 
 __all__ = ['PTBSQLAlchemyJobStore']
