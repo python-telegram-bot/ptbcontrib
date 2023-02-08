8858e5d0caea87b3feee296bb4885b2d23dfb16f
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2023-02-09 01:51:44 +0200

    PTBSQLAlchemyJobStore: Update for v20 changes

--- ptbcontrib/ptb_sqlalchemy_jobstore/jobstore.py
+++ ptbcontrib/ptb_sqlalchemy_jobstore/jobstore.py
@@ -17,14 +17,13 @@
 # You should have received a copy of the GNU Lesser Public License
 # along with this program.  If not, see [http://www.gnu.org/licenses/].
 """This file contains PTBSQLAlchemyJobStore."""
-
 import logging
-from typing import Any
+from importlib import import_module
+from typing import Any, Optional
 
-import telegram
 from apscheduler.job import Job as APSJob
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
-from telegram.ext import CallbackContext, Dispatcher
+from telegram.ext import Application, CallbackContext, Job
 
 logger = logging.getLogger(__name__)
 
@@ -34,10 +33,10 @@ class PTBSQLAlchemyJobStore(SQLAlchemyJobStore):
     Wraps apscheduler.SQLAlchemyJobStore to make :class:`telegram.ext.Job` class storable.
     """
 
-    def __init__(self, dispatcher: Dispatcher, **kwargs: Any) -> None:
+    def __init__(self, application: Application, **kwargs: Any) -> None:
         """
         Args:
-            dispatcher (:class:`telegram.ext.Dispatcher`): Dispatcher instance
+            application (:class:`telegram.ext.Application`): Application instance
                 that will be passed to CallbackContext when recreating jobs.
             **kwargs (:obj:`dict`): Arbitrary keyword Arguments to be passed to
                 the SQLAlchemyJobStore constructor.
@@ -52,7 +51,7 @@ class PTBSQLAlchemyJobStore(SQLAlchemyJobStore):
             )
 
         super().__init__(**kwargs)
-        self.dispatcher = dispatcher
+        self.application = application
 
     def add_job(self, job: APSJob) -> None:
         """
@@ -81,18 +80,31 @@ class PTBSQLAlchemyJobStore(SQLAlchemyJobStore):
         """
         # make new job which is copy of actual job cause
         # modifying actual job also modifies jobs in threadpool
-        # executor which are currently running/going to run and
+        # executor which are currently running/going to run, and
         # we'll get incorrect argument instead of CallbackContext.
         prepped_job = APSJob.__new__(APSJob)
         prepped_job.__setstate__(job.__getstate__())
         # remove CallbackContext from job args since
-        # it includes refrences to dispatcher which
+        # it includes references to application which
         # is unpickleable. we'll recreate CallbackContext
         # in _reconstitute_job method.
-        if isinstance(job.args[0], CallbackContext):
-            tg_job = job.args[0].job
-            # APScheduler stores args as tuple.
-            prepped_job.args = (tg_job.name, tg_job.context)
+        filtered_args: tuple = ()
+        for arg in prepped_job.args:
+            tg_job: Optional[Job] = None
+            if isinstance(arg, Job):
+                tg_job = arg
+            if isinstance(arg, CallbackContext):
+                assert arg.job is not None
+                tg_job = arg.job
+            if tg_job:
+                filtered_args = (
+                    tg_job.name,
+                    tg_job.data,
+                    tg_job.chat_id,
+                    tg_job.user_id,
+                    tg_job.callback.__module__,
+                )
+        prepped_job.args = filtered_args
         return prepped_job
 
     def _reconstitute_job(self, job_state: bytes) -> APSJob:
@@ -101,15 +113,24 @@ class PTBSQLAlchemyJobStore(SQLAlchemyJobStore):
         Args:
             job_state (:obj:`str`): String containing pickled job state.
         """
-        job = super()._reconstitute_job(job_state)  # pylint: disable=W0212
+        job: APSJob = super()._reconstitute_job(job_state)  # pylint: disable=W0212
 
         # Here we rebuild callback context for the job which
         # are going for execution.
-        tg_job = telegram.ext.Job(
-            callback=None,
-            name=job.args[0],
-            context=job.args[1],
+        name, data, chat_id, user_id, module = job.args
+        callback_module = import_module(module)
+        tg_job = Job(
+            callback=getattr(callback_module, name),
+            chat_id=chat_id,
+            user_id=user_id,
+            name=name,
+            data=data,
         )
-        ctx = CallbackContext.from_job(tg_job, self.dispatcher)
-        job._modify(args=(ctx,))  # pylint: disable=W0212
+        ctx = CallbackContext.from_job(tg_job, self.application)
+        job._modify(  # pylint: disable=W0212
+            args=(
+                ctx.job,
+                self.application,
+            )
+        )
         return job
