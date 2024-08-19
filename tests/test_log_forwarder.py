8bc77f6a6f93e1c05d21d9d1df2857b9c5c15f3f
Lurian <50703897+Lur1an@users.noreply.github.com>
2024-08-19 16:11:25 +0200

    Add `log_forwarder` (#113)
    * implement logging forwarder handler
    
    * add mocking test for log forwarder
    
    * correctly format import and type functions for CI lints
    
    * upgrade ptb dep, log_levels no longer mutable default value, link python issue in comment
    
    * fix import ordering
    
    * fix pre-commit issues
    
    * fix flake8 lint
    
    * pre-commit should finally pass

---
+++ tests/test_log_forwarder.py
@@ -0,0 +1,21 @@
+import logging
+from unittest.mock import MagicMock
+
+from telegram.constants import ParseMode
+
+from ptbcontrib.log_forwarder import LogForwarder
+
+
+async def test_log_forwarder():
+    root_logger = logging.getLogger()
+    chat_ids = [69420]
+    bot = MagicMock()
+    log_forwarder = LogForwarder(bot, chat_ids)
+    root_logger.addHandler(log_forwarder)
+
+    logger = logging.getLogger("test_logger")
+    logger.error("TEST")
+
+    bot.send_message.assert_called_with(
+        chat_id=69420, text="```\nTEST\n```", parse_mode=ParseMode.MARKDOWN_V2
+    )
