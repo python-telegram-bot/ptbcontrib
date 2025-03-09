88329f645f1568aac52f230ff540972f51bd3956
David <dsb321mp@gmail.com>
2025-03-09 03:07:09 +0300

    Add `extract_passed_user` Contribution (#124)

---
+++ ptbcontrib/extract_passed_user/extract.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+#
+# A library containing community-based extension for the python-telegram-bot library
+# Copyright (C) 2020-2025
+# The ptbcontrib developers
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser Public License for more details.
+#
+# You should have received a copy of the GNU Lesser Public License
+# along with this program.  If not, see [http://www.gnu.org/licenses/].
+
+"""This module attempts to extract side user passed actual user from incoming message"""
+
+from __future__ import annotations
+
+import asyncio
+import re
+from typing import TYPE_CHECKING, Callable
+
+from telegram import SharedUser
+from telegram.error import TelegramError
+
+from ..username_to_chat_api import UsernameToChatAPI
+
+if TYPE_CHECKING:
+    from telegram import Chat, Message
+
+
+def get_num_from_text(
+    text: str,
+) -> int | None:
+    """Extract first number from text"""
+    if matched_number := re.search(r"\d+", text):
+        return int(matched_number.group())
+    return None
+
+
+def _default_resolver(
+    wrapper: UsernameToChatAPI,
+    username: str,
+) -> Chat | None:
+    """
+    Try to resolve username to Chat object.
+    Catch exceptions from username_to_chat_api ?
+    """
+    return asyncio.run(
+        wrapper.resolve(
+            username=username.strip(),
+        )
+    )
+
+
+async def extract_passed_user(
+    message: Message,
+    username_resolver: Callable | UsernameToChatAPI | None = None,
+) -> SharedUser | None:
+    """
+    4 cases:
+    1. message.users_shared
+    2. message.contact.user_id
+    3. message.text starts with '@' and resolved by wrapper
+    4. message.text has only numbers, resolved by get_num_from_text
+
+    About contact entity:
+        1. `contact` may be without user_id.
+        2. `contact` not contain @name at all, so not convertable to complete SharedUser.
+    """
+    chat = user_id = None
+    if message.users_shared:  # Note: May be select multiple
+        return message.users_shared.users[0]
+    if username_resolver and message.text and (username := re.search(r"@\S+", message.text)):
+        if isinstance(username_resolver, UsernameToChatAPI):
+            chat = _default_resolver(
+                wrapper=username_resolver,
+                username=username.group(),
+            )
+        else:
+            chat: Chat | None = await username_resolver(  # type: ignore[no-redef]
+                username=message.text,
+            )
+    elif message.contact and message.contact.user_id:
+        user_id = message.contact.user_id
+    elif message.text:
+        user_id = get_num_from_text(
+            text=message.text,
+        )
+
+    if user_id:
+        try:
+            chat = await message.get_bot().get_chat(
+                chat_id=user_id,
+            )
+        except TelegramError:  # pragma: no cover
+            return None
+    if chat:
+        return SharedUser(
+            user_id=chat.id,
+            username=chat.username,
+            first_name=chat.first_name,
+            last_name=chat.last_name,
+        )
+    return None
---
+++ tests/test_extract_passed_user.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+#
+# A library containing community-based extension for the python-telegram-bot library
+# Copyright (C) 2020-2025
+# The ptbcontrib developers
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser Public License for more details.
+#
+# You should have received a copy of the GNU Lesser Public License
+# along with this program.  If not, see [http://www.gnu.org/licenses/].
+
+from __future__ import annotations
+
+from datetime import datetime
+from functools import partial
+from typing import TYPE_CHECKING
+from unittest.mock import AsyncMock, create_autospec, patch
+
+import pytest
+from telegram import Chat, Contact, Message, SharedUser, UsersShared
+from telegram.constants import ChatType
+
+from ptbcontrib.extract_passed_user import extract, extract_passed_user, get_num_from_text
+from ptbcontrib.username_to_chat_api import UsernameToChatAPI
+
+if TYPE_CHECKING:
+    from unittest.mock import MagicMock
+
+chat = Chat(
+    id=1,
+    type=ChatType.PRIVATE,
+    username="username",
+    first_name="first_name",
+    last_name="last_name",
+)
+message_fabric = partial(
+    Message,
+    message_id=1,
+    date=datetime.now(),
+    chat=chat,
+)
+shared_user = SharedUser(
+    user_id=1,
+    username=chat.username,
+    first_name=chat.first_name,
+    last_name=chat.last_name,
+)
+
+
+class TestGetNumsFromText:
+    """test_get_nums_from_text"""
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        argnames="text, expected",
+        argvalues=[
+            ("abc123xyz", 123),
+            ("98765", 98765),
+            ("  ab#$@c9def8g7h6 ", 9),
+        ],
+    )
+    def test_success(
+        text: str,
+        expected: int,
+    ):
+        assert (
+            get_num_from_text(
+                text=text,
+            )
+            == expected
+        )
+
+    @staticmethod
+    def test_exceptions():
+        for text in ("abcdef", ""):
+            assert (
+                get_num_from_text(
+                    text=text,
+                )
+                is None
+            )
+
+
+def test_default_resolver():
+    extract._default_resolver(
+        username="username",
+        wrapper=create_autospec(
+            spec=UsernameToChatAPI,
+            spec_set=True,
+            instance=False,
+        ),
+    )
+
+
+@pytest.fixture(
+    scope="function",
+)
+def mock_message():
+    result = create_autospec(
+        spec=message_fabric(
+            text="foo",
+        ),
+        users_shared=None,
+        contact=None,
+        spec_set=True,
+    )
+    result.get_bot.return_value.get_chat = AsyncMock(
+        spec_set=True,
+        return_value=chat,
+    )
+    yield result
+
+
+async def test_shared_user():
+    result = await extract_passed_user(
+        message=message_fabric(
+            users_shared=UsersShared(
+                request_id=1,
+                users=(shared_user,),
+            ),
+        ),
+    )
+    assert result == shared_user
+
+
+async def test_text_username():
+    async def resolver(
+        **_,
+    ):
+        return chat
+
+    result = await extract_passed_user(
+        message=message_fabric(
+            text="  foo 434 @username @second_user ",
+        ),
+        username_resolver=resolver,
+    )
+    assert result == shared_user
+
+
+async def test_text_username_default_resolver():
+    with patch.object(
+        target=extract,
+        attribute="_default_resolver",
+        autospec=True,
+        spec_set=True,
+        return_value=chat,
+    ):
+        result = await extract_passed_user(
+            message=message_fabric(
+                text="  @username ",
+            ),
+            username_resolver=create_autospec(
+                spec=UsernameToChatAPI,
+                spec_set=True,
+                instance=False,
+            ),
+        )
+        assert result == shared_user
+
+
+async def test_contact_with_user_id(
+    mock_message: MagicMock,
+):
+    mock_message.contact = Contact(
+        phone_number="qwerty123",
+        first_name="John",
+        user_id=1,
+    )
+    result = await extract_passed_user(
+        message=mock_message,
+    )
+    assert result == shared_user
+
+
+async def test_contact_without_user_id():
+    result = await extract_passed_user(
+        message=message_fabric(
+            contact=Contact(
+                phone_number="qwerty123",
+                first_name="John",
+            ),
+            users_shared=None,
+        ),
+    )
+    assert result is None
+
+
+async def test_text_with_user_id(
+    mock_message: MagicMock,
+):
+    mock_message.text = " qwerty123 bla bla 456 "
+    result = await extract_passed_user(
+        message=mock_message,
+    )
+    assert result == shared_user
